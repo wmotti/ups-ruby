f10339f063e21668e8493b351c784d4a8b4b0241
Walter Mottinelli <walter.mottinelli@triboo.it>
2018-09-20 10:44:42 +0200

    Add test

--- spec/ups/connection/ship_spec.rb
+++ spec/ups/connection/ship_spec.rb
@@ -21,7 +21,6 @@ describe UPS::Connection do
         shipment_builder.add_service '07'
         shipment_builder.add_description 'Description'
         shipment_builder.add_international_invoice invoice_form
-        #puts shipment_builder.to_xml
       end
     end
 
@@ -69,6 +68,44 @@ describe UPS::Connection do
       subject.wont_equal false
       subject.success?.must_equal false
       subject.error_description.must_equal "Missing or invalid shipper number"
+    end
+  end
+
+  describe "if requesting a return shipment" do
+
+    subject do
+      server.ship do |shipment_builder|
+        shipment_builder.add_access_request ENV['UPS_LICENSE_NUMBER'], ENV['UPS_USER_ID'], ENV['UPS_PASSWORD']
+        shipment_builder.add_shipper shipper
+        shipment_builder.add_ship_from shipper
+        shipment_builder.add_ship_to ship_to
+        shipment_builder.add_sold_to sold_to
+        shipment_builder.add_package package
+        shipment_builder.add_payment_information ENV['UPS_ACCOUNT_NUMBER']
+        shipment_builder.add_service '07'
+        shipment_builder.add_description 'Description'
+        shipment_builder.add_return_service('9', 'UPS Print Return Label (PRL)')
+      end
+    end
+
+    it "should do what ever it takes to get that shipment shipped!" do
+      p subject
+      subject.wont_equal false
+      subject.success?.must_equal true
+    end
+
+    it "should return the label data" do
+      subject.label_graphic_image.must_be_kind_of File
+      subject.label_html_image.must_be_kind_of File
+      subject.label_graphic_extension.must_equal '.gif'
+
+      subject.graphic_image.must_be_kind_of File
+      subject.html_image.must_be_kind_of File
+      subject.graphic_extension.must_equal '.gif'
+    end
+
+    it "should return the tracking number" do
+      subject.tracking_number.must_match(/1Z#{ENV['UPS_ACCOUNT_NUMBER']}\d{10}/)
     end
   end
 end
