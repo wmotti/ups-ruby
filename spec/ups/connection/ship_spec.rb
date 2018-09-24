8666742f10c92a1b323d42f9e828d3e97a0218e3
Walter Mottinelli <walter.mottinelli@triboo.it>
2018-09-24 11:36:52 +0200

    Fixes

--- spec/ups/connection/ship_spec.rb
+++ spec/ups/connection/ship_spec.rb
@@ -11,13 +11,14 @@ describe UPS::Connection do
 
     subject do
       server.ship do |shipment_builder|
+        @account_number = ENV['UPS_IT_ACCOUNT_NUMBER']
         shipment_builder.add_access_request ENV['UPS_LICENSE_NUMBER'], ENV['UPS_USER_ID'], ENV['UPS_PASSWORD']
-        shipment_builder.add_shipper shipper
-        shipment_builder.add_ship_from shipper
+        shipment_builder.add_shipper shipper(@account_number)
+        shipment_builder.add_ship_from shipper(@account_number)
         shipment_builder.add_ship_to ship_to
         shipment_builder.add_sold_to sold_to
         shipment_builder.add_package package
-        shipment_builder.add_payment_information ENV['UPS_ACCOUNT_NUMBER']
+        shipment_builder.add_payment_information @account_number
         shipment_builder.add_service '07'
         shipment_builder.add_description 'Description'
         shipment_builder.add_international_invoice invoice_form
@@ -46,7 +47,7 @@ describe UPS::Connection do
     end
 
     it "should return the tracking number" do
-      subject.tracking_number.must_match(/1Z#{ENV['UPS_ACCOUNT_NUMBER']}\d{10}/)
+      subject.tracking_number.must_match(/1Z#{@account_number}\d{10}/)
     end
   end
 
@@ -54,12 +55,13 @@ describe UPS::Connection do
 
     subject do
       server.ship do |shipment_builder|
+        @account_number = ENV['UPS_IT_ACCOUNT_NUMBER']
         shipment_builder.add_access_request ENV['UPS_LICENSE_NUMBER'], ENV['UPS_USER_ID'], ENV['UPS_PASSWORD']
-        shipment_builder.add_shipper shipper.merge!({shipper_number: 'INVALID'})
-        shipment_builder.add_ship_from shipper
+        shipment_builder.add_shipper shipper('INVALID')
+        shipment_builder.add_ship_from shipper('INVALID')
         shipment_builder.add_ship_to ship_to
         shipment_builder.add_package package
-        shipment_builder.add_payment_information ENV['UPS_ACCOUNT_NUMBER']
+        shipment_builder.add_payment_information @account_number
         shipment_builder.add_service '07'
         shipment_builder.add_description 'Description'
       end
